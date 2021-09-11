6916fec761a239efcd592bc10ae70eee7aa93308
Ahmed .M A. Ghanim <42157141+ham1o7d@users.noreply.github.com>
2021-09-11 23:06:48 +0300

    I delete image from top page.

--- src/component/Nav/Nav.jsx
+++ src/component/Nav/Nav.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 // import {Link} from 'react-router-dom';
-import navImage from '../../assist/netflix-logo.png'
 import './Nav.css'
 
 const Nav = () => {
